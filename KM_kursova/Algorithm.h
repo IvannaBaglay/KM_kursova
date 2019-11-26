7a743e10efdff494701f3f48a1ebe100362136ef
IvannaB <ivankamorkai.baglay@gmail.com>
2019-11-26 21:18:42 +0200

    Added interface in Algorithm

--- KM_kursova/Algorithm.h
+++ KM_kursova/Algorithm.h
@@ -1,8 +1,23 @@
 #ifndef _ALGORITHM_H_
 #define _ALGORITHM_H_
 
-class Algorithm {
+#include "Node.h"
+#include "Channel.h"
 
+class Algorithm {
+public:
+    Algorithm()
+    {
+        /*CreateTable()*/
+    }
+    void Start();
+protected:
+    void CreateTable()
+    {
+
+    }
+private:
+    /*Matrix of Node and Channel (Node*Node) how node are connected*/
 };
 
 #endif // !_ALGORITHM_H_
