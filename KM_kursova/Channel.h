c200d89d7c68adfe1022f0134ff7b183b047f195
IvannaB <ivankamorkai.baglay@gmail.com>
2019-11-26 21:27:17 +0200

    Added interface Channel

--- KM_kursova/Channel.h
+++ KM_kursova/Channel.h
@@ -1,8 +1,25 @@
 #ifndef _CHANNEL_H_
 #define _CHANNEL_H_
 
-class Channel {
+#define DUPLEX 0
+#define HALF_DUPLEX 1
 
+class Channel {
+public:
+    const unsigned int get_weight() const {
+        return weight;
+    }
+    const unsigned int get_type() const {
+        return type;
+    }
+    /*
+    function for display info when we select channel with mouse
+    */
+protected:
+
+private:
+    int weight;
+    unsigned int type;
 };
 
 #endif // !_CHANNEL_H_
